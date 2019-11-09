b60e428ef5ce4d01aee180af0b7013faab4f142d
Sulaxman <sulaxmank@gmail.com>
2019-11-09 08:40:40 +0530

    Commit from developer1 to GitHub

--- online.java
+++ online.java
@@ -3,5 +3,7 @@ class Online
    public static void main(String args[])
    {
      System.out.println("Welcome to GIT Repo");
+     System.out.println("Welcome to DevOps ");
+     # To show the changes in GIT
    }
 }
